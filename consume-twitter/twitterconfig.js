f1df5015ed0ab1797a5bb7dd6a4666f664da12d2
Lucas Jellema <lucas.jellema@amis.nl>
2017-12-13 10:26:38 +0100

    Dummy twitterconfig module

---
+++ consume-twitter/twitterconfig.js
@@ -0,0 +1,10 @@
+// CHANGE THIS **************************************************************
+// go to https://apps.twitter.com/ to register your app
+var twitterconfig = {
+    consumer_key: 'YourKeyBNtLQ9E',
+    consumer_secret: 'YourSecretoHOBS6ikaXlgxBvEiQGx',
+    access_token_key: 'YourAccestokenKeyuzxkGrYfBGLI7Kao9AQT4xjaX9',
+    access_token_secret: 'YourAccesstokenSecretiCx44bvHN'  
+    };
+    
+    module.exports = {twitterconfig};
